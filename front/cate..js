7e5924031f4bc083fcf35bb3abac924c42d1cf19
aya kamal <aya621617@gmil.com>
2024-08-07 00:32:33 -0800

    added js file

---
+++ front/cate..js
@@ -0,0 +1,45 @@
+// let names=['Pizza','Burger','Momo'];
+// // let head=document.createTextNode(names);
+// let imge=document.querySelector(".imge");
+// let photo1=document.getElementById('photo1');
+
+// cont.style.textAlign='center';
+
+// imge.style.width='200px';
+// photo1.style.width='100%';
+// photo1.style.marginTop='5px';
+// imge.style.background='#444';
+// imge.style.color='#fff';
+// imge.style.padding='10px';
+// imge.style.margin='5px';
+// imge.style.display='inline-block';
+
+
+
+
+
+// function element( names)
+// {
+
+// //content
+// let head=document.createTextNode(names);
+
+
+// //style
+
+// imge.style.width='200px';
+// photo1.style.width='100%';
+// imge.style.background='#444';
+// imge.style.color='#fff';
+// imge.style.padding='10px';
+// imge.style.margin='5px';
+// imge.style.display='inline-block';
+
+
+
+// }
+// for(let i=0;i<3;i++)
+// {
+// element(names[i]);
+ 
+// }
