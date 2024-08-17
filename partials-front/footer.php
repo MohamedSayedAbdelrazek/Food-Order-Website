11c317a931ebcea3379abfbfef536c788b62f76c
aya kamal <aya621617@gmil.com>
2024-08-17 15:32:53 -0800

    links

--- partials-front/footer.php
+++ partials-front/footer.php
@@ -24,7 +24,7 @@
 </div>
     <!--foot.................-->
     <div class="end">
-    <p id="p_end">All rights reserved. Designed By <span id="span_end"><a href="###">Mohamed Sayed</a> &<a href="###"> Aya Kamal</a></span></p>
+    <p id="p_end">All rights reserved. Designed By <span id="span_end"><a href="https://www.linkedin.com/in/mohamed-sayed-01b02a287?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Mohamed Sayed</a> &<a href="https://www.linkedin.com/in/aya-kamal-b19b022b0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"> Aya Kamal</a></span></p>
     
     </div>
 
